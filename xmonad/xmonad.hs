b1621a61c195e3c031078d99a9f1f2864dd6ca6b
Jonathan Rascher <jon@bcat.name>
2010-06-27 01:55:41 -0500

    Add configuration files for xmonad, xmobar, and stalonetray

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,70 @@
+import IO
+import XMonad
+import XMonad.Config.Gnome
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Grid
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run
+
+workspaceName "1" = "1-main"
+workspaceName "2" = "2-aux"
+workspaceName "3" = "3-term"
+workspaceName "4" = "4-write"
+workspaceName "5" = "5-media"
+workspaceName "6" = "6-comm"
+workspaceName "7" = "7-mail"
+workspaceName "8" = "8-winxp"
+workspaceName "9" = "9-temp"
+workspaceName x   = x
+
+isSplash =
+    isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
+
+main = do
+    xmobar <- spawnPipe "xmobar ~/.xmobarrc"
+
+    xmonad $ ewmh defaultConfig
+        { terminal        = "gnome-terminal"
+        , layoutHook      = layoutHintsWithPlacement (0.5, 0.5) $
+                            avoidStruts $
+                            smartBorders $
+                            Tall 1 (3 / 100) (56 / 100)
+                        ||| Tall 1 (3 / 100) (1 / 2)
+                        ||| Mirror (Tall 1 (3 / 100) (1 / 2))
+                        ||| ThreeCol 1 (3 / 100) (-1 / 3)
+                        ||| Full
+        , manageHook      = (className =? "stalonetray" --> doIgnore)
+                        <+> (className =? "Bsnes" --> doCenterFloat)
+                        <+> (className =? "Gcalctool" -->doCenterFloat)
+                        <+> (className =? "Totem" --> doCenterFloat)
+                        <+> (title =? "glxgears" --> doCenterFloat)
+                        <+> (isSplash --> doIgnore)
+                        <+> manageDocks
+                        <+> (manageHook defaultConfig)
+        , handleEventHook = fullscreenEventHook
+                        <+> handleEventHook defaultConfig
+        , modMask         = mod4Mask
+        , logHook         = dynamicLogWithPP xmobarPP
+                                { ppCurrent         = xmobarColor "yellow" ""
+                                                    . wrap "[" "]"
+                                                    . workspaceName
+                                , ppVisible         = xmobarColor "yellow" ""
+                                                    . wrap "(" ")"
+                                                    . workspaceName
+                                , ppHidden          = xmobarColor "white" ""
+                                                    . wrap "{" "}"
+                                                    . workspaceName
+                                , ppHiddenNoWindows = wrap "<" ">"
+                                                    . workspaceName
+                                , ppTitle           = xmobarColor "green" ""
+                                                    . shorten 255
+                                , ppOutput          = hPutStrLn xmobar }
+                         >> setWMName "LG3D"
+                         >> logHook defaultConfig
+        , startupHook     = gnomeRegister }
