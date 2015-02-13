4e0f170610593f76828d73c7a7887f9058866621
Jonathan Rascher <jon@bcat.name>
2015-02-12 22:10:19 -0500

    Remove XMonad workspace names to save pager space

--- config/taffybar/taffybar.hs
+++ config/taffybar/taffybar.hs
@@ -6,17 +6,6 @@ import System.Taffybar.Systray
 import System.Taffybar.TaffyPager
 
 main = do
-    let workspaceName "1" = "1-uno"
-        workspaceName "2" = "2-dos"
-        workspaceName "3" = "3-tres"
-        workspaceName "4" = "4-gimp"
-        workspaceName "5" = "5-media"
-        workspaceName "6" = "6-chat"
-        workspaceName "7" = "7-bkrg"
-        workspaceName "8" = "8-virt"
-        workspaceName "9" = "9-temp"
-        workspaceName x   = x
-
     let pager = taffyPagerNew defaultPagerConfig
             { activeWindow     = colorize "#94d900" ""
                                . escape
@@ -25,26 +14,21 @@ main = do
                                . colorize "#8ccdf0" ""
                                . escape
                                . wrap "[" "]"
-                               . workspaceName
             , hiddenWorkspace  = wrap "<b>" "</b>"
                                . escape
                                . wrap "{" "}"
-                               . workspaceName
             , emptyWorkspace   = wrap "<b>" "</b>"
                                . colorize "#816749" ""
                                . escape
                                . wrap "<" ">"
-                               . workspaceName
             , visibleWorkspace = wrap "<b>" "</b>"
                                . colorize "#8ccdf0" ""
                                . escape
                                . wrap "(" ")"
-                               . workspaceName
             , urgentWorkspace  = wrap "<b>" "</b>"
                                . colorize "#1f1912" "#ec6c99"
                                . escape
-                               . wrap "*" "*"
-                               . workspaceName }
+                               . wrap "*" "*" }
         music = mpris2New
         tray  = systrayNew
         time  = textClockNew Nothing "<span color='#d5fac8'>%l:%M %p</span> " 1
