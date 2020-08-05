f04680b66acf93d1125c7499a881edacdab572e0
Jonathan Rascher <jon@bcat.name>
2020-08-05 12:58:26 -0500

    Still more prompt fixin'

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -1,11 +1,10 @@
 # Disable variable expansion in prompt strings.
 shopt -u promptvars
 
-# Determine if we have tput to generate terminal escape codes, falling back to
-# no terminal colors/attributes if we can't find tput.
+# If possible, use tput to generate terminal escape codes for colors/attributes.
 if type tput >/dev/null 2>&1; then
   _bash_prompt_num_colors=$(tput colors)
-  _bash_prompt_term_reset=$(tput sgr0)
+  _bash_prompt_color_reset=$(tput sgr0)
 
   _bash_prompt_title_start=$(tput tsl)
   _bash_prompt_title_end=$(tput fsl)
@@ -15,50 +14,36 @@ if type tput >/dev/null 2>&1; then
   # indicating the title can be set with the following escape sequence:
   #
   # OSC 2 ; $window_title BEL
-  if [[ (-z $_bash_prompt_title_start || -z $_bash_prompt_title_end) ]] && \
+  if [[ -z $_bash_prompt_title_start || -z $_bash_prompt_title_end ]] && \
       tput XT 2>/dev/null; then
     _bash_prompt_title_start=$'\e]2;'
     _bash_prompt_title_end=$'\a'
   fi
-else
-  _bash_prompt_num_colors=-1
-  _bash_prompt_term_reset=
 fi
 
 # Set up terminal colors for various bits of the prompt. These look best with a
 # color palette matching my Vim theme <https://github.com/bcat/abbott.vim>;
 # however, these color choices should look fine with any reasonable palette.
-# Note that we intentionally don't use color 12 (bold color 4) because it looks
-# very low-contrast in the Linux console color palette.
 if (( _bash_prompt_num_colors >= 16 )); then
   # For terminals that support high-intensity colors (i.e., terminals supporting
   # at least 16 total colors), use those directly.
-  _bash_prompt_term_red=$(tput setaf 9)
-  _bash_prompt_term_green=$(tput setaf 10)
-  _bash_prompt_term_yellow=$(tput setaf 11)
-  _bash_prompt_term_magenta=$(tput setaf 13)
-  _bash_prompt_term_cyan=$(tput setaf 14)
-  _bash_prompt_term_white=$(tput setaf 15)
+  _bash_prompt_color_red=$(tput setaf 9)
+  _bash_prompt_color_green=$(tput setaf 10)
+  _bash_prompt_color_yellow=$(tput setaf 11)
+  _bash_prompt_color_magenta=$(tput setaf 13)
+  _bash_prompt_color_cyan=$(tput setaf 14)
+  _bash_prompt_color_white=$(tput setaf 15)
 elif (( _bash_prompt_num_colors >= 8 )); then
   # For terminals that only support 8 colors, use the bold attribute, which many
   # terminals (including the Linux console) will render as bright.
-  _bash_prompt_term_bold=$(tput bold)
-
-  _bash_prompt_term_red=$_bash_prompt_term_bold$(tput setaf 1)
-  _bash_prompt_term_green=$_bash_prompt_term_bold$(tput setaf 2)
-  _bash_prompt_term_yellow=$_bash_prompt_term_bold$(tput setaf 3)
-  _bash_prompt_term_magenta=$_bash_prompt_term_bold$(tput setaf 5)
-  _bash_prompt_term_cyan=$_bash_prompt_term_bold$(tput setaf 6)
-  _bash_prompt_term_white=$_bash_prompt_term_bold$(tput setaf 7)
-else
-  # For terminals with less than the standard 8 colors, render in monochrome.
-  _bash_prompt_term_red=
-  _bash_prompt_term_green=
-  _bash_prompt_term_green=
-  _bash_prompt_term_yellow=
-  _bash_prompt_term_cyan=
-  _bash_prompt_term_magenta=
-  _bash_prompt_term_white=
+  _bash_prompt_bold=$(tput bold)
+  _bash_prompt_color_red=$_bash_prompt_bold$(tput setaf 1)
+  _bash_prompt_color_green=$_bash_prompt_bold$(tput setaf 2)
+  _bash_prompt_color_yellow=$_bash_prompt_bold$(tput setaf 3)
+  _bash_prompt_color_magenta=$_bash_prompt_bold$(tput setaf 5)
+  _bash_prompt_color_cyan=$_bash_prompt_bold$(tput setaf 6)
+  _bash_prompt_color_white=$_bash_prompt_bold$(tput setaf 7)
+  unset _bash_prompt_bold
 fi
 
 # Cache some information that shouldn't vary from prompt to prompt.
@@ -72,7 +57,7 @@ preexec () {
   local cmd_line job cmd
 
   # Split the command line into multiple words.
-  read -a cmd_line <<<"$1"
+  read -ra cmd_line <<<"$1"
 
   # Find job specification.
   case "${cmd_line[0]}" in
@@ -120,17 +105,17 @@ precmd () {
 
   # Set the terminal title and reset text attributes for user input.
   _bash_prompt_ps1_escape "$(_bash_prompt_title)"
-  _bash_prompt_ps1_escape "$_bash_prompt_term_reset"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_reset"
 }
 
 _bash_prompt_ps1_build_machine () {
-  _bash_prompt_ps1_escape "$_bash_prompt_term_green"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_green"
   _bash_prompt_ps1_append "$USER"
-  _bash_prompt_ps1_escape "$_bash_prompt_term_white"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_white"
   _bash_prompt_ps1_append @
-  _bash_prompt_ps1_escape "$_bash_prompt_term_magenta"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_magenta"
   _bash_prompt_ps1_append "$_bash_prompt_host"
-  _bash_prompt_ps1_escape "$_bash_prompt_term_white"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_white"
 }
 
 _bash_prompt_ps1_build_jobs () {
@@ -139,18 +124,19 @@ _bash_prompt_ps1_build_jobs () {
 
   _bash_prompt_ps1_append ' J:'
   if (( num_jobs == 0 )); then
-    _bash_prompt_ps1_escape "$_bash_prompt_term_green"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_green"
   elif (( num_jobs == 1 )); then
-    _bash_prompt_ps1_escape "$_bash_prompt_term_yellow"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_yellow"
   else
-    _bash_prompt_ps1_escape "$_bash_prompt_term_red"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_red"
   fi
   _bash_prompt_ps1_append "$num_jobs"
-  _bash_prompt_ps1_escape "$_bash_prompt_term_white"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_white"
 }
 
 _bash_prompt_ps1_build_git() {
   [[ -n $_bash_prompt_have_git ]] || return;
+
   local status
   status=$(git status --porcelain -b 2>/dev/null) || return
 
@@ -179,29 +165,29 @@ _bash_prompt_ps1_build_git() {
   _bash_prompt_ps1_append ' G:'
   if [[ $branch_info =~ \[(ahead|behind)\ [[:digit:]]+\] ]]; then
     if [[ ${BASH_REMATCH[1]} == behind ]]; then
-      _bash_prompt_ps1_escape "$_bash_prompt_term_red"
+      _bash_prompt_ps1_escape "$_bash_prompt_color_red"
     else
-      _bash_prompt_ps1_escape "$_bash_prompt_term_yellow"
+      _bash_prompt_ps1_escape "$_bash_prompt_color_yellow"
     fi
   else
-    _bash_prompt_ps1_escape "$_bash_prompt_term_green"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_green"
   fi
   _bash_prompt_ps1_append "$branch_name$status_char"
-  _bash_prompt_ps1_escape "$_bash_prompt_term_white"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_white"
 }
 
 _bash_prompt_ps1_build_directory () {
-  _bash_prompt_ps1_escape "$_bash_prompt_term_cyan"
+  _bash_prompt_ps1_escape "$_bash_prompt_color_cyan"
   _bash_prompt_ps1_append ' '
   _bash_prompt_ps1_append "$_bash_prompt_path" truncate
 }
 
 _bash_prompt_ps1_build_symbol () {
   if (( UID == 0 )); then
-    _bash_prompt_ps1_escape "$_bash_prompt_term_red"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_red"
     _bash_prompt_ps1_append '# '
   else
-    _bash_prompt_ps1_escape "$_bash_prompt_term_white"
+    _bash_prompt_ps1_escape "$_bash_prompt_color_white"
     _bash_prompt_ps1_append '$ '
   fi
 }
